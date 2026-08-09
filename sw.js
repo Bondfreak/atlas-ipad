689b96a1c9a31f50181893a7dbba379a7c9290f7
Bondfreak <68965971+Bondfreak@users.noreply.github.com>
2026-08-09 14:21:53 +0200

    Atlas v0.3.7: bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "atlas-ipad-alpha-v0.3.6";
+const CACHE = "atlas-ipad-alpha-v0.3.7";
 const ASSETS = ["./","./index.html","./manifest.json","./Hero-v2.png","./cooling-system.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
