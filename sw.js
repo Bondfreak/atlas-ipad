1ec4a1684d0fd631db420bad0ef1c89ff91bdfac
Bondfreak <68965971+Bondfreak@users.noreply.github.com>
2026-08-09 10:42:42 +0200

    Replace damaged cooling graphic with intact original

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "atlas-ipad-alpha-v0.3.1-epa0005-png";
+const CACHE = "atlas-ipad-alpha-v0.3.2-epa0005-image-fix";
 const ASSETS = ["./","./index.html","./manifest.json","./Hero-v2.png","./cooling-system.png","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
